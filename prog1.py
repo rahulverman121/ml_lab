14bd19db18d23e1a6de3337ecbe63e26023903b9
rahulverman121 <98263712+rahulverman121@users.noreply.github.com>
2022-11-09 15:48:17 +0530

    Create prog1.py

---
+++ prog1.py
@@ -0,0 +1,22 @@
+import csv
+with open('data.csv', 'r') as f: 
+    reader = csv.reader(f)
+    headers = next(reader) 
+    your_list = list(reader)
+h = [['0', '0', '0', '0', '0', '0']]
+for i in your_list: 
+    print(i)
+    if i[-1] == "TRUE":
+       j = 0
+       for x in i:
+         
+         if x != "TRUE":
+           if x != h[0][j] and h[0][j] == '0': 
+                    h[0][j] = x
+           elif x != h[0][j] and h[0][j] != '0': 
+             h[0][j] = '?'
+           else:
+             pass       
+    j = j + 1
+print("The maximally specific hypothesis for a given training example is: ") 
+print(h)
