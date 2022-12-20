04a2954521be98877fcbc38f7b92266a50551521
rahulverman121 <98263712+rahulverman121@users.noreply.github.com>
2022-12-20 22:11:59 +0530

    Update prog8.py

--- prog8.py
+++ prog8.py
@@ -34,3 +34,14 @@ print(q['heartdisease'])
 print('\n2. Probability of HeartDisease given chol (Cholestoral) =100')
 q = HeartDisease_infer.query(variables=['heartdisease'], evidence={'chol': 100})
 print(q['heartdisease'])
+
+'''Dataset:
+P7_data7_names.csv (14 attributes)
+age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak, slope,ca,thal,heartdisease
+P7_data7_heart.csv (5 instances out of 303)
+63.0,1.0,1.0,145.0,233.0,1.0,2.0,150.0,0.0,2.3,3.0,0.0,6.0,0
+67.0,1.0,4.0,160.0,286.0,0.0,2.0,108.0,1.0,1.5,2.0,3.0,3.0,2
+67.0,1.0,4.0,120.0,229.0,0.0,2.0,129.0,1.0,2.6,2.0,2.0,7.0,1
+37.0,1.0,3.0,130.0,250.0,0.0,0.0,187.0,0.0,3.5,3.0,0.0,3.0,0
+41.0,0.0,2.0,130.0,204.0,0.0,2.0,172.0,0.0,1.4,1.0,0.0,3.0,0
+'''
